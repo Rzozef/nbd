4e32949dd57ce951865ebfb070a05c85897a7d8b
Piotr Gągorowski <236533@edu.p.lodz.pl>
2022-11-22 21:30:07 +0100

    fix of last commit

--- src/main/java/org/pl/databaseRepository/ClientRedisRepository.java
+++ src/main/java/org/pl/databaseRepository/ClientRedisRepository.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.pl.converters.ClientRedisConverter;
 import org.pl.databaseModel.ClientRedis;
+import org.pl.exceptions.ClientException;
 import org.pl.model.Client;
 
 import java.util.ArrayList;
@@ -44,8 +45,21 @@ public class ClientRedisRepository extends RedisRepository {
         return clientsRedis;
     }
 
-    public ClientRedis set(String key, ClientRedis client) throws JsonProcessingException {
-        getPool().set(hashPrefix + key, objectMapper.writeValueAsString(client));
+    public ClientRedis set(String key, ClientRedis clientRedis) throws JsonProcessingException, ClientException {
+        Client client = ClientRedisConverter.fromRepositoryModelClient(clientRedis);
+        Client client1 = ClientRedisConverter.fromRepositoryModelClient(read(key));
+        client1.setArchive(client.isArchive());
+        client1.setBalance(client.getBalance());
+        client1.setFirstName(client.getFirstName());
+        client1.setLastName(client.getLastName());
+        client1.setPhoneNumber(client.getPhoneNumber());
+        client1.setClientType(client.getClientType());
+        client1.setAddress(client.getAddress());
+        if (client.getRepairs() > client.getClientType().getMaxRepairs()) {
+            throw new ClientException(ClientException.CLIENT_MAX_REPAIRS_EXCEEDED);
+        }
+        client1.setRepairs(client.getRepairs());
+        getPool().set(hashPrefix + key, objectMapper.writeValueAsString(ClientRedisConverter.toRepositoryModel(client1)));
         return objectMapper.readValue(getPool().get(hashPrefix + key), ClientRedis.class);
     }
 
@@ -59,5 +73,9 @@ public class ClientRedisRepository extends RedisRepository {
 
     public void deleteAll() {
         getJedis().flushDB();
+    }
+
+    public boolean isConnected() {
+        return getJedis().ping().equalsIgnoreCase("pong");
     }
 }
